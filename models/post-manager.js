6f4ff085caf3a700f0398448ab7321a348d32198
Farzan <farzankhalili01@gmail.com>
2024-06-29 14:51:45 +0330

    Remove test code

--- models/post-manager.js
+++ models/post-manager.js
@@ -2,7 +2,6 @@ const pool = require('./mysql_manager')
 const tableName = process.env.POST_TABLE
 //********************************************/
 function addPost(date, user, text, callback) {
-    user = 'testUser'// NOTE: for test purpose, to be removed
     pool.query(`INSERT INTO ${tableName} (date, username, text) VALUES ('${date}', '${user}', '${text}')`, (err,res)=>{
         callback(err, res)
         }
